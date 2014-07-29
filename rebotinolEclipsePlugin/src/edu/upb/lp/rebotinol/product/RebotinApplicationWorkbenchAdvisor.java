7c3ce2471456d3169da3882bdc7fbe49c134df7a
Alexis Marechal <epeo22@gmail.com>
2014-07-29 14:58:28 -0400

    Saving workbench before exiting the app.

--- rebotinolEclipsePlugin/src/edu/upb/lp/rebotinol/product/RebotinApplicationWorkbenchAdvisor.java
+++ rebotinolEclipsePlugin/src/edu/upb/lp/rebotinol/product/RebotinApplicationWorkbenchAdvisor.java
@@ -3,6 +3,7 @@ package edu.upb.lp.rebotinol.product;
 import java.net.URL;
 
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.resource.ImageDescriptor;
@@ -22,14 +23,15 @@ public class RebotinApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
 	private static final String PERSPECTIVE_ID = "rebotinolEclipsePlugin.perspectives.mainPerspective"; //$NON-NLS-1$
 
-    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
-        return new RebotinApplicationWorkbenchWindowAdvisor(configurer);
-    }
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new RebotinApplicationWorkbenchWindowAdvisor(configurer);
+	}
 
 	public String getInitialWindowPerspectiveId() {
 		return PERSPECTIVE_ID;
 	}
-	
+
 	@Override
 	public IAdaptable getDefaultPageInput() {
 		return ResourcesPlugin.getWorkspace().getRoot();
@@ -37,35 +39,42 @@ public class RebotinApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
 	@Override
 	public void initialize(IWorkbenchConfigurer configurer) {
-		//see http://stackoverflow.com/questions/8277902/add-package-explorer-in-rcp-application-result-in-loosing-some-icon
+		// see
+		// http://stackoverflow.com/questions/8277902/add-package-explorer-in-rcp-application-result-in-loosing-some-icon
 		configurer.setSaveAndRestore(true);
-	     PlatformUI.getPreferenceStore().setValue(
-	            IWorkbenchPreferenceConstants.SHOW_TRADITIONAL_STYLE_TABS, false);
-	     
-	     IDE.registerAdapters();
-	     
-	     String ICONS_PATH = "icons/full/";
-		Bundle ideBundle = Platform.getBundle(IDEWorkbenchPlugin.IDE_WORKBENCH);
-	     
-	     declareWorkbenchImage(
-	             configurer, 
-	             ideBundle,
-	             IDE.SharedImages.IMG_OBJ_PROJECT, 
-	             ICONS_PATH + "obj16/prj_obj.gif",
-	             true);
+		PlatformUI.getPreferenceStore().setValue(
+				IWorkbenchPreferenceConstants.SHOW_TRADITIONAL_STYLE_TABS,
+				false);
 
-	     declareWorkbenchImage(
-	             configurer, 
-	             ideBundle,
-	             IDE.SharedImages.IMG_OBJ_PROJECT_CLOSED, 
-	             ICONS_PATH + "obj16/cprj_obj.gif", 
-	             true);
+		IDE.registerAdapters();
+
+		String ICONS_PATH = "icons/full/";
+		Bundle ideBundle = Platform.getBundle(IDEWorkbenchPlugin.IDE_WORKBENCH);
+
+		declareWorkbenchImage(configurer, ideBundle,
+				IDE.SharedImages.IMG_OBJ_PROJECT, ICONS_PATH
+						+ "obj16/prj_obj.gif", true);
+
+		declareWorkbenchImage(configurer, ideBundle,
+				IDE.SharedImages.IMG_OBJ_PROJECT_CLOSED, ICONS_PATH
+						+ "obj16/cprj_obj.gif", true);
 	}
-	
-	private void declareWorkbenchImage(IWorkbenchConfigurer configurer_p, Bundle ideBundle, String symbolicName, String path, boolean shared)  
-	{
-	    URL url = ideBundle.getEntry(path);
-	    ImageDescriptor desc = ImageDescriptor.createFromURL(url);
-	    configurer_p.declareImage(symbolicName, desc, shared);
+
+	private void declareWorkbenchImage(IWorkbenchConfigurer configurer_p,
+			Bundle ideBundle, String symbolicName, String path, boolean shared) {
+		URL url = ideBundle.getEntry(path);
+		ImageDescriptor desc = ImageDescriptor.createFromURL(url);
+		configurer_p.declareImage(symbolicName, desc, shared);
+	}
+
+	@Override
+	public boolean preShutdown() {
+		try {
+			// save the full workspace before quit
+			ResourcesPlugin.getWorkspace().save(true, null);
+		} catch (final CoreException e) {
+			throw new IllegalStateException(e);
+		}
+		return super.preShutdown();
 	}
 }
